c615b3e9e847d4f19f7bb304c9254820b5454c49
MiAwan <malikirfanofficial3332@gmail.com>
2023-02-28 14:16:51 +0500

    Team Page Design

--- src/views/teams/Teams.js
+++ src/views/teams/Teams.js
@@ -1,9 +1,59 @@
-import React from 'react'
 
-const Teams = () => {
+import {
+  CAvatar,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CDropdown,
+  CDropdownMenu,
+  CDropdownItem,
+  CDropdownToggle,
+  CWidgetStatsA,
+} from '@coreui/react'
+
+import React from 'react';
+
+const Card = () => {
   return (
-    <div>Teams</div>
-  )
+    <div className="card">
+     
+      <div className="card-body">
+        
+        <a href="#" className="btn btn-primary" style={{ marginLeft: '85%' }}>Add a Team</a>
+        <CTable align="middle" className="mb-0 border" hover responsive style={{ marginTop: '20px' }}>
+                <CTableHead color="light" >
+                  <CTableRow>
+                    
+                    <CTableHeaderCell className="text-center">
+                    </CTableHeaderCell>
+                    
+                    <CTableHeaderCell>Name</CTableHeaderCell>
+                    <CTableHeaderCell className="text-center"></CTableHeaderCell>
+                    <CTableHeaderCell className="text-center">Members</CTableHeaderCell>
+                    <CTableHeaderCell className="text-center"></CTableHeaderCell>
+                    <CTableHeaderCell className="text-center">Projects</CTableHeaderCell>
+                    <CTableHeaderCell className="text-center"></CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  
+                </CTableBody>
+              </CTable>
+      </div>
+    </div>
+  );
 }
 
-export default Teams
+export default Card;
