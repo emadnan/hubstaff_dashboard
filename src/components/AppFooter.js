b35b6c5d5aa739942d57d2dae653b352311639c7
jahanzaibbaig <jahanzaib.baig36@gmail.com>
2023-05-19 16:56:51 +0500

    Styling added to footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,17 +2,41 @@ import React from 'react'
 import { CFooter } from '@coreui/react'
 
 const AppFooter = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <CFooter>
+    <CFooter
+      style={{ backgroundColor: '#f8f9fa', color: '#333', padding: '20px', marginTop: '20px' }}
+    >
       <div>
-        <a href="https://www.biafotech.com/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.biafotech.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            textDecoration: 'none',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            color: 'black',
+          }}
+        >
           BiafoTech
         </a>
-        <span className="ms-1">&copy; 2022 Private Lmtd.</span>
+        <span className="ms-1">&copy; {currentYear} Private Lmtd.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://www.biafotech.com/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.biafotech.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            textDecoration: 'none',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            color: 'black',
+          }}
+        >
           BiafoTech
         </a>
       </div>
