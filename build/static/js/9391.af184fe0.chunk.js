ae195426ab15ad0cc7fbe2c0e67022ebedcd88f5
Adil01012000 <115777936+Adil01012000@users.noreply.github.com>
2023-07-13 12:21:38 +0500

    Git Ignore changes

---
+++ build/static/js/9391.af184fe0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_coreui_coreui_free_react_admin_template=self.webpackChunk_coreui_coreui_free_react_admin_template||[]).push([[9391],{19391:function(e,r,t){t.r(r);t(72791);var c=t(80184);r.default=function(){return(0,c.jsx)("div",{children:"Allreports"})}}}]);
+//# sourceMappingURL=9391.af184fe0.chunk.js.map
