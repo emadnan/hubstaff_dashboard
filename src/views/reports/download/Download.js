ea796f73a42fe6fbf127a0f43867a22f4e949404
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-05-02 13:00:03 +0500

    Update Download.js

--- src/views/reports/download/Download.js
+++ src/views/reports/download/Download.js
@@ -13,7 +13,8 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 import { CTable, CTableBody, CTableHead, CTableHeaderCell, CTableRow } from '@coreui/react'
-import { DatePicker, Card, Select, Form } from 'antd'
+import { DatePicker,Select, Form } from 'antd'
+import { Card } from '@mui/material'
 import { saveAs } from 'file-saver'
 import json2csv from 'json2csv'
 import moment from 'moment'
@@ -21,6 +22,8 @@ import dayjs from 'dayjs'
 import PictureAsPdfSharpIcon from '@mui/icons-material/PictureAsPdfSharp'
 import html2pdf from 'html2pdf.js'
 import { Button } from 'antd'
+import NoRecordsMessegeComponent from 'src/components/noRecordsMessegeComponent/NoRecordsMessegeComponent'
+import InitialMessegeForCompany from 'src/components/intialMessegeForCompany/InitialMessegeForCompany'
 const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export default function Download() {
@@ -30,6 +33,9 @@ export default function Download() {
         width: '240px',
         fontWeight: 'bold',
       }
+    const cardStyle = {
+        width: '100%',
+    }
       
     const [isEmployeeSelected, setIsEmployeeSelected] = useState(false)
     const [selectedDate, setSelectedDate] = useState(null)
@@ -44,9 +50,13 @@ export default function Download() {
 
         // Array Declaration for API Calls
         const [users, setUsers] = useState([])
-        const [dayendreport, setDayendReport] = useState([])
+        const [report, setReport] = useState([])
         const [user_id, setUserId] = useState('')
         const [notfoundmessage, setNotFoundMessage] = useState(true)
+        const [offline , setOffline] = useState(false)
+        const [online , setOnline] = useState(false)
+        const [all_users , setAllUsers] = useState(false)
+        const [default_api , setDefaultApi] = useState(true)
     
         // Images URLs State
         const [imagesUrls, setImagesUrls] = useState([])
@@ -75,17 +85,17 @@ export default function Download() {
 
         async function getReport(date) {
         try {
-            const response = await fetch(`${BASE_URL}/api/getDayEndReportById/${user_id}/${date}`);
+            const response = await fetch(`${BASE_URL}/api/get-daily-report-of-users-for-teamlead/${user_id}/${date}`);
 
             const data = await response.json();
 
-            if (data.dayEndReport.length === 0) {
+            if (data.data.length === 0) {
                 setNotFoundMessage(true);
             } else {
                 setNotFoundMessage(false);
                 console.log('Data', data)
-                setDayendReport(data.dayEndReport);
-                console.log('Report', dayendreport);
+                setReport(data.data);
+                console.log('Report', report);
             }
         } catch (error) {
             console.log(error);
@@ -93,17 +103,22 @@ export default function Download() {
     }
 
         const onDateChange = (date, dateString) => {
-            setSelectedDate(dateString)
             setIsAdminLogin(true)
             setSelectedDate(dateString)
-            // getReport(dateString)
-            console.log(dateString);
+            getReport(dateString)
         }
 
       const handleExport = () => {
         console.log('EXCEL Download');
       }
+   // Static Messeges
+   const renderInitialMessage = () => {
+    return <InitialMessegeForCompany />
+}
 
+const renderNoRecordFoundMessage = () => {
+    return <NoRecordsMessegeComponent />
+}
     return (
         <>
         {/* <Box>
@@ -135,10 +150,10 @@ export default function Download() {
             </Box>
         </Box> */}
         <Box>
-                <h2>Day-End Reports</h2>
+                <h2>Download Reports</h2>
                 <br></br>
                 <div className="row mt-2 mb-2 justify-content-between">
-                    <div className="col-md-4">
+                    <div className="col-md-6">
                         <div className="d-flex align-items-center">
                             <DatePicker
                                 value={selectedDate ? dayjs(selectedDate, 'YYYY-MM-DD') : null}
@@ -150,7 +165,24 @@ export default function Download() {
                                     marginRight: '10px'
                                 }}
                             />
-                            <Button onClick={onTodayButtonClicked}>
+                            <Form
+                                style={{
+                                    width: '100%',
+                                }}
+                            >
+                                <Select placeholder="Filter Members...">
+                                    <Select.Option>
+                                        All Members
+                                    </Select.Option>
+                                    <Select.Option>
+                                        Online Members
+                                    </Select.Option>
+                                    <Select.Option>
+                                        Offline Members
+                                    </Select.Option>
+                                </Select>
+                            </Form>
+                            <Button  style={{ width: '100%', }}  onClick={onTodayButtonClicked}>
                             Get Report
                             </Button>
                            
@@ -161,63 +193,77 @@ export default function Download() {
                     </Button>
                 </div>
 
-                    {/* <div className="col-md-4">
-                        {perm.some((item) => item.name === 'All_Data') || perm.some((item) => item.name === 'Company_Data') ? (
-                            <div className="d-flex align-items-center">
-                                <Form.Item
-                                    name="select"
-                                    hasFeedback
-                                    style={{
-                                        width: '100%',
-                                    }}
-                                >
-                                    <Select
-                                        placeholder="Members"
-                                        onChange={handleUserChange}
-                                        value={user_id}
-                                        showSearch
-                                        filterOption={(input, option) =>
-                                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                                        }
-                                    >
-                                        {users.map((user) => (
-                                            <Select.Option value={user.id} key={user.id}>
-                                                {user.name}
-                                            </Select.Option>
-                                        ))}
-                                    </Select>
-                                </Form.Item>
-                            </div>
-                        ) : null}
-                    </div>
-                </div>
 
-                <Divider />
 
                 <div>
                     {
-                        user_id ? (
+                        selectedDate ? (
                             notfoundmessage ? (
-                                renderNoRecordFoundMessage()
+                                <Box mt={5}>
+                                <Card style={cardStyle}>
+                                  <Box className="row">
+                                    <Typography variant="h6" sx={{ color: '#9E9E9E', textAlign: 'center' }}>
+                                      NO RECORDS FOUND
+                                    </Typography>
+                                  </Box>
+                                </Card>
+                              </Box>
                             ) : (
+                            <Box mt={5}>
                                 <Card>
-                                    <div className="report-card">
+                                    <div className="report-card ">
                                         <h4 className='mt-3 ml-5'>Report Summary</h4>
                                         <br></br>
-                                        {dayendreport.map((data, index) => (
-                                            <div key={index} className="report-item ml-5 mt-1">
-                                                <span>&#8227;</span>  {data.day_report}
-                                            </div>
+                                            <div className="report-item ml-5 mt-1">
+                                                {/* <span>&#8227;</span>  {data.name} */}
+                                                <Table>
+                                                    <TableHead>
+                                                        <TableCell>
+                                                            Name
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            Email
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            Online Time
+                                                        </TableCell>
+                                                    </TableHead>
+                                                    <TableBody>
+                                        {report.map((data, index) => (
+                                                        <TableRow key={index}>
+                                                            <TableCell>
+                                                                {data.name}
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {data.email}
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {data.totalHours}:{data.totalMinutes}:{data.totalSeconds}
+                                                            </TableCell>
+                                                        </TableRow>
                                         ))}
+
+                                                    </TableBody>
+                                                </Table>
+                                            </div>
                                         <br></br>
                                     </div>
                                 </Card>
+                            </Box>
                             )
                         ) : (
-                            renderInitialMessage()
+                            <Box mt={5}>
+                                <Card style={cardStyle}>
+                                <Box className="row">
+                                    <Typography variant="h6" sx={{ color: '#9E9E9E', textAlign: 'center' }} className='p-2'>
+                                    PLEASE SELECT THE DATE TO GENERATE THE REPORT
+                                    </Typography>
+                                </Box>
+                                </Card>
+                            </Box>
                         )
                     }
-                </div> */}
+                </div>
 
             </Box>
             </>
