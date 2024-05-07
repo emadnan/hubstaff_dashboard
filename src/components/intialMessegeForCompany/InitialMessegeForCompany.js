5dbaa87d3d4048845cc11fba10b938eb65d2669d
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-05-07 10:59:31 +0500

    Update InitialMessegeForCompany.js

--- src/components/intialMessegeForCompany/InitialMessegeForCompany.js
+++ src/components/intialMessegeForCompany/InitialMessegeForCompany.js
@@ -14,7 +14,7 @@ export default function InitialMessegeForCompany() {
         <Card style={cardStyle}>
           <Box className="row">
             <Typography variant="h6" sx={{ color: '#9E9E9E', textAlign: 'center' }}>
-              PLEASE SELECT THE EMPLOYEE NAME FOR WHOM YOU WISH TO GENERATE THE REPORT
+              PLEASE SELECT THE EMPLOYEE TO GENERATE REPORT
             </Typography>
           </Box>
         </Card>
